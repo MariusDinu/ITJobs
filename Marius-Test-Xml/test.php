43d3aff5694e111584b4f61b421933de7439791d
Marius <mariusd30@yahoo.com>
2020-08-21 01:59:32 +0300

    final test

--- Marius-Test-Xml/test.php
+++ Marius-Test-Xml/test.php
@@ -9,8 +9,8 @@ $items=simplexml_load_file("test.xml") or die("Error: Cannot create object");
 $i=0;
 
 foreach ($items as $item){
-
-    print '</br>';
+if($i<5)
+ {   print '</br>';
     $i=$i+1;
 
     $question =$item[0]->question;
@@ -21,35 +21,54 @@ foreach ($items as $item){
    
 
     print "<div id='divFormQuestion$i'>"; 
-    print "<P>";print $question;print "</p>";
-    print "<INPUT TYPE = 'Radio' Name ='$i' value= '1' >"; print $ans1;print "";
-    print "<INPUT TYPE = 'Radio' Name ='$i' value= '2' >";print $ans2;print "";
-    print "<INPUT TYPE = 'Radio' Name ='$i' value= '3' >";print $ans3;print "";
-    print"<INPUT TYPE = 'Radio' Name ='$i' value= '4' >"; print $ans4;print "";
-    print "</div>";
+    print "<P>$i.";print $question;print "</p>";
+    print "<p><INPUT TYPE = 'Radio' Name ='$i' value= '1' >"; print $ans1;print "</p>";
+    print "<p><INPUT TYPE = 'Radio' Name ='$i' value= '2' >";print $ans2;print "</p>";
+    print "<p><INPUT TYPE = 'Radio' Name ='$i' value= '3' >";print $ans3;print "</p>";
+    print "<p><INPUT TYPE = 'Radio' Name ='$i' value= '4' >"; print $ans4;print "</p>";
+    print "</div>";}
 }
 
 ?>
 
-
+<p id="errorTest"></p>
 <button  id="submitTest" name="submitTest" onclick="result()"  >Submit</button>
 </form>
 <script>
-var user=<?php $_SESSION['user']?>;
+<?php 
+if(isset($_SESSION['user'])){ $a=$_SESSION['user'];print "var user=$a";}
+?>
 var rate=0;
 function result(){
 var children = document.getElementById('formaIntrebari').childNodes;
 var raspunsuri=document.getElementsByTagName('input');
-for(let raspuns of raspunsuri)
+if(parseInt(checkAllInput())==parseInt(raspunsuri.length/4))
+{for(let raspuns of raspunsuri)
 if(raspuns.checked)
 {
     rate=parseInt(rate)+parseInt(raspuns.value);
      
 }
+document.getElementById('errorTest').innerHTML="";
 //script pentru trimiterea in baza de date
 console.log(rate); 
-rate=0;
+rate=0;}
+else {document.getElementById('errorTest').innerHTML="completeaza tot!";}
 event.preventDefault();
+}
+
+
+
+function checkAllInput(){
+var check=0;
+var raspunsuri=document.getElementsByTagName('input');
+
+for(let raspuns of raspunsuri)
+if(raspuns.checked)
+{
+check=parseInt(check)+1;
+}
+return check;
 }
 </script>
 
