db5fda2bbae8c9a750c0371a188f92aa11f73e7b
Marius <mariusd30@yahoo.com>
2020-08-22 01:20:20 +0300

    delete useless files

--- Raluca-Test-XML/form.php
+++
@@ -1,9 +0,0 @@
-<FORM NAME ="form1" METHOD ="POST" ACTION ="punctaj.php">
-
-<P><?PHP print $question; ?>
-<P><INPUT TYPE = 'Radio' Name ="q" value= '1' checked="true"><?PHP print 'Sunt de acord'; ?>
-<P><INPUT TYPE = 'Radio' Name ="q" value= '2' ><?PHP print 'Partial de acord'; ?>
-<P><INPUT TYPE = 'Radio' Name ="q" value= '3' ><?PHP print 'Nu stiu ce sa raspund'; ?>
-<P><INPUT TYPE = 'Radio' Name ="q" value= '4' ><?PHP print 'Nu sunt de acord'; ?>
-
-</FORM>
--- Raluca-Test-XML/items.php
+++
@@ -1,8 +0,0 @@
-<?php
-$items=simplexml_load_file("testMBTI (1).xml") or die("Error: Cannot create object");
-
-$_SESSION["punctaj"]=0;
-
-//print($items->item[0]->question['index']);
-
-?>
