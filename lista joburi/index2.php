67af80ca351cc1ef79a1aa762b7192e3c1523040
Octavian Iacob <octavian.iacob@icloud.com>
2020-08-29 20:48:22 +0300

    futura font

--- lista joburi/index2.php
+++ lista joburi/index2.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>it-jobs</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../public/styles.css">
 </head>
 
 <body onload="abc()">
