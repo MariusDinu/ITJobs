022f63793a347a7adf34b52cfffb1dd45887b8b7
cimpean98 <49518517+cimpean98@users.noreply.github.com>
2020-09-01 18:53:29 +0300

    Add files via upload

--- User/ScriptsUser/CVScript.php
+++ User/ScriptsUser/CVScript.php
@@ -1,32 +1,24 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Completare CV</title>
-</head>
-<body>
-
-<?php
 include "../../DB.php";
 $DB=new DB();
-$limbaje=$_SESSION["limbaje"];				//aici preiau datele din formular.. o sa modific cu session cand va fi nevoie..
-$tehnologii=$_SESSION["tehnologii"];		//it's just for testing
-$joburi=$_SESSION["joburi"];				//cand refac aici o sa pun si validari/restrictii
-$expLimbaj=$_SESSION["expLimbaj"];
-$expJob=$_SESSION["expJob"];
+$limbaje=$_POST["limbaje"];				
+$tehnologii=$_POST["tehnologii"];		
+$joburi=$_POST["joburi"];				
+$nume=$_POST["nume"];
+$prenume=$_POST["prenume"];
+$nastere=$_POST["nastere"];
+$gen=$_POST["gen"];
+$email=$_POST["email"];
+$telefon=$_POST["phone"];
+$descriere=$_POST["descriere"];
+$sql="INSERT INTO `cv`( `Nume`, `Prenume`, `Data_nastere`, `Descriere`, `Email`, `Telefon`, `Gen`, `Limbaje`, `Joburi`, `Tehnologii`) VALUES ('$nume','$prenume','$nastere','$descriere','$email','$telefon','$gen','$limbaje','$joburi','$tehnologii')";
 
-$sql="INSERT INTO `cv`(`limbaje`,`expLimbaj`,`joburi`,`expJob`,`tehnologii`) VALUES ('$limbaje','$expLimbaj','$joburi','$expJob','$tehnologii')";
+$q=$DB::obtine_conexiune()->prepare($sql);
+if($q->execute())
+	{echo "true";}
+else
+	{echo "false";}
 
 
-$q=$DB::obtine_conexiune()->prepare($sql);	//fac conexiunea la baza de date
-
-
-if ($q->execute()) {
-  echo "Ai completat CV-ul cu success!"; session_destroy();
-} else {
-  echo "Eroare.";		
-}
 ?>
-</html>
