f12751cf4042dbbcae7ed26f3efb1563c1c2460d
cimpean98 <49518517+cimpean98@users.noreply.github.com>
2020-09-02 17:37:52 +0300

    Update index.php

--- Employer/ListaJoburiEmployer/index.php
+++ Employer/ListaJoburiEmployer/index.php
@@ -43,7 +43,7 @@ session_start();
       </a>
 
       <a href='#' id='lang' class='flex items-center py-2 hover:text-secondary md:mx-5'>
-        <img src='../img/ro-flag.png' alt='' class='h-5'>
+        <img src='../../img/ro-flag.png' alt='' class='h-5'>
         <svg viewBox='0 0 20 20' fill='currentColor' class='w-6 h-6 chevron-down'>
           <path fill-rule='evenodd' d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z' clip-rule='evenodd'></path>
         </svg>
